d115ac78b8f0879e3f27081d024c62ca9749fc0f
Mitesh Shah <Mr.Miteshah@gmail.com>
2012-05-09 15:45:28 +0530

    Now this scripts checks every 10 minutes for any authentication failure message founded on /var/log/auth.log file and update twitter status.

--- SecurityAlert.sh
+++ SecurityAlert.sh
@@ -14,33 +14,33 @@
 # https://plus.google.com/111560558537332305125/posts/S6mrHJeS5Wd
 
 
-# Checking every hour for any Authentication Failure entry founded on
-# /var/log/auth.log file and update twitter status
-
 # You should need to create a crontab entry for this script
-# So this script runs every hours automatically
+# So this script runs every 10 minutes automatically.
+
+# Checking for any authentication failure entry founded on
+# /var/log/auth.log file and update twitter status.
+
 
 #Checking twidge (command-line twitter client) is installed or not
 dpkg --list | grep twidge &> /dev/null
 
 if [ $? -ne 0 ]
 then
-echo "Twidge (command-line twitter client) is not installed "
-echo "For how to install and configure twidge check the following links:"
-echo "http://www.howtogeek.com/62018/how-to-use-the-linux-terminal-to-update-twitter/"
+	echo "Twidge (command-line twitter client) is not installed "
+	echo "For how to install and configure twidge check the following links:"
+	echo "http://www.howtogeek.com/62018/how-to-use-the-linux-terminal-to-update-twitter/"
 else
-#Calculate 1 hour ago time in unix time format
-HOURAGO=$(date +%s --date "1 hour ago")
+	#Calculate 10 Minutes ago time in unix time format
+	TENMINAGO=$(date +%s --date "10 minute ago")
 
-# Take the last authentication failure entry from auth.log
-SMS=$(cat /var/log/auth.log | grep 'authentication failure' | tail -n1 )
+	# Take the last authentication failure entry from auth.log
+	SMS=$(cat /var/log/auth.log | grep 'authentication failure' | tail -n1 )
 
-# Extract the time from SMS variable and convert it to unix time format
-SMSTIME=$(date -d "$(echo $SMS | awk '{print $1,$2,$3}')" +%s)
+	# Extract the time from SMS variable and convert it to unix time format
+	SMSTIME=$(date -d "$(echo $SMS | awk '{print $1,$2,$3}')" +%s)
 
-if [ $SMSTIME -gt $HOURAGO ]; then
-echo "Updating Twitter Status..."
-echo $SMS | awk '{print $3,$5,$9,$10,$12,$14,$15,$16}' | twidge update
+	if [ $SMSTIME -gt $TENMINAGO ]; then
+	echo "Updating Twitter Status..."
+	echo $SMS | awk '{print $3,$5,$9,$10,$12,$14,$15,$16}' | twidge update
+	fi
 fi
-fi
-
